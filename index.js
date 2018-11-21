63a8d06d4c12b7601bd1b277795b0be764fd84fe
Jackson Sorrells <sorrelj@purdue.edu>
2018-11-21 18:21:51 -0500

    added materialize-css

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express();
+const path = require('path')
+
+app.engine('html',require('ejs').renderFile)
+app.set('view engine','html');
+
+
+/*
+ * / get request
+ * 
+ */
+app.get('/', (req, res) => {
+    console.log('/ get request');
+
+    return res.render(path.join(__dirname+'/views/index.html'))
+
+});
+
+
+
+
+
+/*
+ * App listen port: 8000
+ * 
+ */
+app.listen(8000, () => {
+    console.log("App started on port: 8000")
+});
