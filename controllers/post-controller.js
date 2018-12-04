e282b7843f460df9708f91b64f8fa6822c64bb88
Jackson Sorrells <sorrelj@purdue.edu>
2018-12-04 03:40:45 -0500

    user posting updated

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -8,40 +8,26 @@ var connection = con.connection;
 module.exports.post = function(req,res){
     console.log('POST request - post');
 
-    console.log(req.body);
-    console.log(req.session.userid)
-    return res.redirect('/home');
+    
 
-    var username = req.body.username;
     var uid = req.session.userid;
+    var currDate = new Date();
 
-    var runsql = 'SELECT friends FROM users WHERE id = \''+uid+'\'';
+    var postData ={
+        "id": uid,
+        "textData": req.body.textdata,
+        "spotify": req.body.embedcode,
+        "created": currDate,
+        "title": req.body.title
+    }
+    
 
-    connection.query(runsql, function(error,results,fields){
+    connection.query('INSERT INTO users SET ?',postData, function(error,results,fields){
         if (error){
             return res.redirect('/error?status=Internal Server Error')
         }
 
-        var newJSON = [];
-        var resp;
-        if (results[0].friends == null){
-            newJSON.push(username);
-
-            resp = JSON.stringify(newJSON);
-        }else{
-            newJSON = JSON.parse(results[0].friends);
-            newJSON.push(username);
-
-            resp = JSON.stringify(newJSON);
-        }
-
-        var q2 = 'UPDATE users SET friends=\''+resp+'\' WHERE id='+uid;
-        connection.query(q2, function(error,results,fields){
-            if (error){
-                return res.redirect('/error?status=Internal Server Error')
-            }
-            return res.redirect('/home?message=Following '+username);
-        });
+        return res.redirect('/home?message=Song Posted!');
 
     });
 
